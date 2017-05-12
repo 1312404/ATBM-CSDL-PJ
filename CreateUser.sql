c7aadcbcabc8e4b545b4a6df4255a8ac72c0f5ee
Thái Quỳnh Nhi <quynhnhi220895@gmail.com>
2017-05-12 00:27:56 -0600

    Create CreateUser.sql

---
+++ CreateUser.sql
@@ -0,0 +1,55 @@
+-- tạo tài khoản cho 36 nhân viên
+create user nv01 identified by nv01;
+create user nv02 identified by nv02;
+create user nv03 identified by nv03;
+create user nv04 identified by nv04;
+create user nv05 identified by nv05;
+create user nv06 identified by nv06;
+create user nv07 identified by nv07;
+create user nv08 identified by nv08;
+create user nv09 identified by nv09;
+create user nv10 identified by nv10;
+create user nv11 identified by nv11;
+create user nv12 identified by nv12;
+create user nv13 identified by nv13;
+create user nv14 identified by nv14;
+create user nv15 identified by nv15;
+create user nv16 identified by nv16;
+create user nv17 identified by nv17;
+create user nv18 identified by nv18;
+create user nv19 identified by nv19;
+create user nv20 identified by nv20;
+create user nv21 identified by nv21;
+create user nv22 identified by nv22;
+create user nv23 identified by nv23;
+create user nv24 identified by nv24;
+create user nv25 identified by nv25;
+create user nv26 identified by nv26;
+create user nv27 identified by nv27;
+create user nv28 identified by nv28;
+create user nv29 identified by nv29;
+create user nv30 identified by nv30;
+create user nv31 identified by nv31;
+create user nv32 identified by nv32;
+create user nv33 identified by nv33;
+create user nv34 identified by nv34;
+create user nv35 identified by nv35;
+create user nv36 identified by nv36;
+
+-- tạo tài khoản cho ít nhất 5 vị trí 
+-- trưởng dự án
+create user  UTrgDuAn identified by truongduan;
+
+-- trưởng phòng
+create user UTrgPhg identified by truongphong;
+
+-- trưởng chi nhánh
+create user UTrgChiNhanh identified by truongchinhanh;
+
+-- nhân viên
+create user UNhanVien identified by nhanvien;
+
+-- giám đốc
+create user UGiamDoc identified by giamdoc;
+
+-- tạo ít nhất 5 dòng cho các bảng dữ liệu còn lại 
