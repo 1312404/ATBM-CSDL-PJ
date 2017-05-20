3fb6f84c9eefdc8b05a8f94542dee74f6c87f8e5
Thái Quỳnh Nhi <quynhnhi220895@gmail.com>
2017-05-20 05:11:54 -0600

    Editing

--- VPD.sql
+++ VPD.sql
@@ -2,7 +2,7 @@
 -- Only them can view thier informations and others in thier department, and viewing thier salaries
 
 -- Create VPD Policy Function 
-create or replace function viewInformAndSalary(p_schema varchar2, p_obj varchar2) 
+create or replace function view_Inform_Salary(p_schema varchar2, p_obj varchar2) 
   return varchar2 
   as 
     UserID varchar2(200);
@@ -10,7 +10,7 @@ create or replace function viewInformAndSalary(p_schema varchar2, p_obj varchar2
     UserID := SYS_CONTEXT('USERENV','SESSION_USER');
     if(UserID like 'nv%') then
       return 
-        'Employee.EmployeeID in (select EmployeeID from Atbmhttt_Lab01.Employee where EmployeeID = ''' || SYS_CONTEXT('USERENV','SESSION_USER') || '''' || ')'; 
+        'Employee.EmployeeID in (select EmployeeID from Atbmhttt_Lab01.Employee where EmployeeID = ''' || UserID || '''' || ')'; 
     else
       return '';
   end;
@@ -21,9 +21,9 @@ dbms_rls.add_policy
 (
   object_schema     => 'Atbmhttt_Lab01', 
   object_name       => 'Employee', 
-  policy_name       => 'viewInformAndSalaryVPD', 
+  policy_name       => 'view_Inform_Salary_VPD', 
   function_schema   => 'Atbmhttt_Lab01', 
-  policy_function   => 'viewInformAndSalary', 
+  policy_function   => 'view_Inform_Salary', 
   statement_types   => 'select, update, insert, delete', 
   sec_relevant_cols => 'Salary',
   update_check      => TRUE
@@ -35,7 +35,7 @@ dbms_rls.drop_policy
 (
   object_schema => 'Atbmhttt_Lab01', 
   object_name   => 'Employee', 
-  policy_name   => 'viewInformAndSalaryVPD'
+  policy_name   => 'view_Inform_Salary_VPD'
 );
 end;
 
